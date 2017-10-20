a2c2eb48efbfdee2c193d0b316b7ae7be678b4a4
cuiziwei <cuiziwei1992@qq.com>
2017-10-20 17:56:56 +0800

    update

--- work/wwwroot/index.php
+++ work/wwwroot/index.php
@@ -1,3 +1,42 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Docker LNMP</title>
+	<style type="text/css">
+		html{
+			font-family:sans-serif;-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%
+		}
+		.mine{
+			font-size:30px;margin-top:200px;
+			color:#333;
+		}
+		footer{
+			text-align: center;
+		}
+		footer a{
+			color:#fff;
+			background: #F95445;
+			display: block;
+			width: 150px;
+			margin:0 auto;
+			text-decoration: none;
+			line-height: 40px;
+			height: 40px;
+			border-radius: 3px;
+		}
+	</style>
+</head>
+<body>
 
-phpinfo();
+<p align="center" class="mine">欢迎使用 Docker LNMP</p>
+
+<footer>
+	<a href="https://github.com/beautysoft/docker-lnmp" target="_blank">查看说明文档</a>
+
+	<p style="color:#ccc;font-size:12px;margin-top:100px;">
+		&copy; BeautySoft 出品
+	</p>
+</footer>
+
+</body>
+</html>
